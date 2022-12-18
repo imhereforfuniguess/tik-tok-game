a9be277acb032b743ce8d9e08a48db163824fa92
imhereforfuniguess <106017717+imhereforfuniguess@users.noreply.github.com>
2022-12-18 21:42:02 +0200

    Change gameBoard into IIFE, add eventListener to it

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-gameBoard = {
-    board: [1,2,1,1,2,1,1,0,1],
-    updateBoard: function () {
+const gameBoard = (function () {
+    const board = [1,2,1,1,2,1,1,0,1]
+    const updateBoard =  () => {
         for (let i = 0; i < 9; i++){
             let cell = document.querySelector(`[data-cell="${i}"]`)
-            cell.textContent = gameBoard.board[i]
+            cell.textContent = board[i]
         }
     }
-}
+
+    cell = document.querySelectorAll(".cell")
+    cell.forEach((cell) => {
+        cell.addEventListener('click', () => {
+            console.log(cell.getAttribute('data-cell'))
+            if (cell.textContent == 0){
+                board[cell.getAttribute('data-cell')] = 1 
+            }
+            if (cell.textContent == 1){
+                board[cell.getAttribute('data-cell')] = 2 
+            }
+            if (cell.textContent == 2){
+                board[cell.getAttribute('data-cell')] = 1 
+            }
+            updateBoard()
+        })
+    })
+
+    return {updateBoard}
+})()
 
 const gameState = (function () {
     let currentPlayer = 1
@@ -27,19 +46,3 @@ const {changeTurn} = gameState
 gameBoard.updateBoard()
 
     
-cell = document.querySelectorAll(".cell")
-cell.forEach((cell) => {
-    cell.addEventListener('click', () => {
-        console.log(cell.getAttribute('data-cell'))
-        if (cell.textContent == 0){
-            gameBoard.board[cell.getAttribute('data-cell')] = 1 
-        }
-        if (cell.textContent == 1){
-            gameBoard.board[cell.getAttribute('data-cell')] = 2 
-        }
-        if (cell.textContent == 2){
-            gameBoard.board[cell.getAttribute('data-cell')] = 1 
-        }
-        gameBoard.updateBoard()
-    })
-})
