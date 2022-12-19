e00e69da4fab8bdde0a64f201050005ca3ea0af2
imhereforfuniguess <106017717+imhereforfuniguess@users.noreply.github.com>
2022-12-19 22:23:37 +0200

    Add function to find a winner

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const gameBoard = (function () {
-    
     // Board array
-    const board = [0,0,0,0,0,0,0,1,2]
+    const board = [0,0,0,0,0,0,0,0,0]
 
     // Updates based on array
     const updateBoard =  () => {
@@ -12,6 +11,7 @@ const gameBoard = (function () {
     }
     updateBoard()
 
+
     // Add clicking
     // Implement clicking based on game state
     cell = document.querySelectorAll(".cell")
@@ -19,17 +19,22 @@ const gameBoard = (function () {
         cell.addEventListener('click', () => {
             if (cell.textContent == 0){
                 board[cell.getAttribute('data-cell')] = gameState.changeTurn()
+                gameState.checkForWin()
+
             }
-            console.log()
-            
             updateBoard()
         })
     })
 
+    return {board}
+
 })()
 
 const gameState = (function () {
+    // Have to start from opposite player cause you change even at first turn
     let currentPlayer = 2
+
+    // Change player
     const changeTurn = () => {
         if (currentPlayer == 1){
             currentPlayer = 2
@@ -38,10 +43,46 @@ const gameState = (function () {
         }
         return currentPlayer
     }
-    return {changeTurn, currentPlayer}
 
-    // I don't know what to do here
+    // Win / Draw check
+    const checkForWin = () => {
+        let checkSum = 0
+
+        // Compares sums to 
+        const compareCheckSum = () => {
+            if (checkSum == 3){
+                console.log(`player 2 wins`)
+            }
+            if (checkSum == -6){
+                console.log(`player 1 wins`)
+            }
+            checkSum = 0
+        }
+
+        // Check who wins rows and columns 
+        for (let i = 0; i < 9; i+=3){
+            for (let j = 0; j < 3; j++){
+                checkSum += gameBoard.board[i+j] * gameBoard.board[i+j] - 3
+            }
+            compareCheckSum()
+            for (let j = 0; j < 3; j++){
+                checkSum += gameBoard.board[i/3+j*3] * gameBoard.board[i/3+j*3] - 3
+            }     
+            compareCheckSum()    
+        }
+
+        // Check who wins diagonals
+        for (let i = 0; i < 9; i+=4){
+            checkSum += gameBoard.board[i] * gameBoard.board[i] - 3
+        }
+        compareCheckSum() 
+        for (let i = 2; i < 8; i+=2){
+            checkSum += gameBoard.board[i] * gameBoard.board[i] - 3
+        }
+        compareCheckSum() 
+    }
+
+
+
+    return {changeTurn, currentPlayer, checkForWin}
 })()
-
-
-    
